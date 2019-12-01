20c618a6daf7bd1a90718486403bac950cb4033c
antongoransson <anton.goransson95@gmail.com>
2019-12-01 06:35:46 +0100

    Add day01

--- day01/day01.py
+++ day01/day01.py
@@ -1,23 +1,22 @@
-from collections import defaultdict
-import regex as re
+def solve_part_1(masses):
+   return sum([mass // 3 - 2 for mass in masses]) 
 
+def solve_part_2(masses):
+   return sum([calc_fuel(mass) for mass in masses])
 
-def solve_part_1():
-   pass
-
-
-def solve_part_2():
-   pass
-
+def calc_fuel(mass):
+    fuel = mass // 3 - 2
+    if fuel <= 0:
+        return 0
+    return fuel + calc_fuel(fuel)
 
 def main():
     with open('input.txt') as f:
-        in_data = list(map(int, re.findall(r'\d+', f)))
-    sol1 =  solve_part_1()
-    print('Part 1: {}'.format(sol1))
-    sol2 = solve_part_2()
-    print('Part 2: {}'.format(sol2))
-
+        in_data = list(map(int, [mass for mass in f]))
+        sol1 =  solve_part_1(in_data)
+        print(f'Part 1: {sol1}')
+        sol2 = solve_part_2(in_data)
+        print(f'Part 2: {sol2}')
 
 if __name__ == "__main__":
     main()
