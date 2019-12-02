0a7364f96969fe66aa5c91a9fa5e1cf2171d1ea1
antongoransson <anton.goransson95@gmail.com>
2019-12-02 17:55:14 +0100

    Cleanup day02

--- day02/day02.py
+++ day02/day02.py
@@ -3,12 +3,10 @@ from itertools import combinations
 
 def addr(a, b, c, registers):
     registers[int(c)] = registers[int(a)] + registers[int(b)]
-    return registers[int(c)]
 
 
 def mulr(a, b, c, registers):
     registers[int(c)] = registers[int(a)] * registers[int(b)]
-    return registers[int(c)]
 
 
 op_codes = {
@@ -17,42 +15,35 @@ op_codes = {
 }
 
 
-def solve_part_1(registers):
+def run_program(noun, verb, registers):
+    registers[1] = noun
+    registers[2] = verb
     i = 0
-    registers[1] = 12
-    registers[2] = 2
     while registers[i] != 99:
-        a = registers[i + 1]
-        b = registers[i + 2]
-        c = registers[i + 3]
-        op_codes[registers[i]](a, b, c, registers)
+        op, a, b, c = registers[i:i + 4]
+        op_codes[op](a, b, c, registers)
         i += 4
-    return registers[0]
+    return registers
+
+
+def solve_part_1(registers):
+    return run_program(12, 2, registers[:])[0]
 
 
 def solve_part_2(default_state):
-    test_range = [i for i in range(100)]
-    for x, y in combinations(test_range, 2):
-        registers = list(default_state)
-        registers[1] = x
-        registers[2] = y
-        i = 0
-        while registers[i] != 99:
-            a = registers[i + 1]
-            b = registers[i + 2]
-            c = registers[i + 3]
-            op_codes[registers[i]](a, b, c, registers)
-            i += 4
+    val_range = [i for i in range(100)]
+    for x, y in combinations(val_range, 2):
+        registers = run_program(x, y, default_state[:])
         if registers[0] == 19690720:
             return registers[1] * 100 + registers[2]
 
 
 def main():
     with open('input.txt') as f:
-        in_data = list((map(int, f.read().strip().split(","))))
-        sol1 = solve_part_1(list(in_data))
+        in_data = list(map(int, f.read().strip().split(",")))
+        sol1 = solve_part_1(in_data)
         print('Part 1: {}'.format(sol1))
-        sol2 = solve_part_2(in_data)
+        sol2 = solve_part_2(list(in_data))
         print('Part 2: {}'.format(sol2))
 
 
