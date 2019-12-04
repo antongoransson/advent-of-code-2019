66673268e3d77b7086d229d6be8d4d4950830b1e
antongoransson <anton.goransson95@gmail.com>
2019-12-04 10:01:34 +0100

    Add day04

--- day04/day04.py
+++ day04/day04.py
@@ -1,19 +1,33 @@
-from collections import defaultdict
-import regex as re
+
+def valid_pairs(pw):
+    matching_digits = []
+    for i in range(1, len(pw)):
+        if pw[i - 1] is pw[i]:
+            matching_digits.append(pw[i])
+        if pw[i - 1] > pw[i]:
+            return []
+    return matching_digits
 
 
 def solve_part_1():
-   pass
+    nbr_valid = 0
+    for i in range(388888, 799999 + 1):
+        if len(valid_pairs(str(i))) >= 1:
+            nbr_valid += 1
+    return nbr_valid
 
 
 def solve_part_2():
-   pass
+    nbr_valid = 0
+    for i in range(388888, 799999 + 1):
+        pairs = valid_pairs(str(i))
+        if len([d for d in pairs if pairs.count(d) == 1]) >= 1:
+            nbr_valid += 1
+    return nbr_valid
 
 
 def main():
-    with open('input.txt') as f:
-        in_data = list(map(int, re.findall(r'\d+', f)))
-    sol1 =  solve_part_1()
+    sol1 = solve_part_1()
     print('Part 1: {}'.format(sol1))
     sol2 = solve_part_2()
     print('Part 2: {}'.format(sol2))
