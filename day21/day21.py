084b1864b7fc1f7dd4193a8617d538f826c6655c
antongoransson <anton.goransson95@gmail.com>
2019-12-21 12:44:26 +0100

    Add day21

--- day21/day21.py
+++ day21/day21.py
@@ -1,21 +1,61 @@
-from collections import defaultdict
-import regex as re
+import os
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from intcode.IntcodeComputer import IntcodeComputer
 
 
-def solve_part_1():
-   pass
+def to_ascii(s):
+    return [(ord(c)) for c in s] + [ord("\n")]
 
+def solve_part_1(instructions):
+    spring_code = [
+        "NOT A T",
+        "NOT B J",
+        "OR T J",
+        "NOT C T",
+        "OR T J",
+        "AND D J",
+        "WALK"
+    ]
+    spring_code_ascii = to_ascii("\n".join(spring_code))
 
-def solve_part_2():
-   pass
+    def get_input():
+        return spring_code_ascii.pop(0)
+    program = IntcodeComputer(instructions, get_input)
+    while not program.done:
+        c = program.run()
+    return c
+
+def solve_part_2(instructions):
+    spring_code = [
+        "NOT A T",
+        "NOT B J",
+        "OR T J",
+        "NOT C T",
+        "OR T J",
+        "AND D J",
+        "NOT E T",
+        "NOT T T",
+        "OR H T",
+        "AND T J",
+        "RUN"
+    ]
+    spring_code_ascii = to_ascii("\n".join(spring_code))
+
+    def get_input():
+        return spring_code_ascii.pop(0)
+        
+    program = IntcodeComputer(instructions, get_input)
+    while not program.done:
+        c = program.run()
+    return c
 
 
 def main():
     with open('input.txt') as f:
-        in_data = list(map(int, re.findall(r'\d+', f)))
-    sol1 =  solve_part_1()
+        data = list(map(int, f.read().strip().split(",")))
+    sol1 =  solve_part_1(data)
     print('Part 1: {}'.format(sol1))
-    sol2 = solve_part_2()
+    sol2 = solve_part_2(data)
     print('Part 2: {}'.format(sol2))
 
 
