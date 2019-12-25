aec58876552f738322e3b39ec10f34ccfacbffa8
antongoransson <anton.goransson95@gmail.com>
2019-12-25 16:33:18 +0100

    Add day25

--- day25/day25.py
+++ day25/day25.py
@@ -1,22 +1,58 @@
-from collections import defaultdict
-import regex as re
+import os
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from intcode.IntcodeComputer import IntcodeComputer
 
 
-def solve_part_1():
-   pass
+def to_ascii(p_input):
+    return [ord(c) for c in "\n".join(p_input)] + [ord("\n")]
 
 
-def solve_part_2():
-   pass
+def solve_part_1(instructions):
+    p_input = []
+    p_input = ["east",
+               "east",
+               "take fixed point",
+               "north",
+               "take spool of cat6",
+               "west",
+               "take shell",
+               "east",
+               "south",
+               "west",
+               "south",
+               "west",
+               "north",
+               "take candy cane",
+               "south",
+               "east",
+               "north",
+               "west",
+               "north",
+               "north",
+               "east",
+               "south"
+               ]
+    p_input = to_ascii(p_input)
+    def get_input():
+        if not p_input:
+            i = input()
+            p_input.extend(list(i))
+            p_input.append("\n")
+        print(chr(p_input[0]), end ='')
+        return p_input.pop(0)
+
+    program = IntcodeComputer(instructions, get_input)
+    while not program.done:
+        o = program.run()
+        if o is None:
+            break
+        print(chr(o), end='')
 
 
 def main():
     with open('input.txt') as f:
-        in_data = list(map(int, re.findall(r'\d+', f)))
-    sol1 =  solve_part_1()
-    print('Part 1: {}'.format(sol1))
-    sol2 = solve_part_2()
-    print('Part 2: {}'.format(sol2))
+        data = list(map(int, f.read().strip().split(",")))
+    solve_part_1(data)
 
 
 if __name__ == "__main__":
